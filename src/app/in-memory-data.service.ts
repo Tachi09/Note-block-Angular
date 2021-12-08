cd247b02a12d24f8abcf7d8232271f800918dc76
Martin Hochman <Hochman19@seznam.cz>
2021-12-08 21:12:24 +0100

    Přidání datumu vytvoření

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,12 +8,12 @@ import { Note } from './note';
 export class InMemoryDataService implements InMemoryDbService{
   createDb() {
     const notes = [
-    {id:1, name: 'pzn č.1', text: 'test1', creation_date: '1.1.2002', edit_date: 'none'},
-    {id:2, name: 'pzn č.2', text: 'test2', creation_date: '2.1.2002', edit_date: 'none'},
-    {id:3, name: 'pzn č.3', text: 'test3', creation_date: '3.1.2002', edit_date: 'none'},
-    {id:4, name: 'pzn č.4', text: 'test4', creation_date: '4.1.2002', edit_date: 'none'},
-    {id:5, name: 'pzn č.5', text: 'test5', creation_date: '5.1.2002', edit_date: 'none'},
-    {id:6, name: 'pzn č.6', text: 'test6', creation_date: '6.1.2002', edit_date: 'none'},
+    {id:1, name: 'pzn č.1', text: 'test1', creation_date: 'January 4, 2019 10:13:00', edit_date: ''},
+    {id:2, name: 'pzn č.2', text: 'test2', creation_date: 'February 7, 2017 10:13:00', edit_date: ''},
+    {id:3, name: 'pzn č.3', text: 'test3', creation_date: 'March 25, 2015 10:13:00', edit_date: ''},
+    {id:4, name: 'pzn č.4', text: 'test4', creation_date: 'April 1, 2013 10:13:00', edit_date: ''},
+    {id:5, name: 'pzn č.5', text: 'test5', creation_date: 'August 20, 2011 10:13:00', edit_date: ''},
+    {id:6, name: 'pzn č.6', text: 'test6', creation_date: 'December 24, 2010 10:13:00', edit_date: ''},
     ];
     return {notes};
   }
